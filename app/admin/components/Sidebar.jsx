a1a694e84576a1b8393a67f08da6e8b3ac3e8e78
Abhijeet_15 <118336271+AbhijeetKamalekar15@users.noreply.github.com>
2025-06-18 15:11:05 +0530

    logout added!!

--- app/admin/components/Sidebar.jsx
+++ app/admin/components/Sidebar.jsx
@@ -7,12 +7,17 @@ import {
   LibraryBig,
   LogOut,
   PackageOpen,
+  ShieldCheck,
   ShoppingCart,
   Star,
   User,
 } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import toast from "react-hot-toast";
+import { signOut } from "firebase/auth";
+import {auth} from "../../../lib/firestore/firebase";
+
 
 export default function Sidebar() {
   const menuList = [
@@ -56,6 +61,11 @@ export default function Sidebar() {
       link: "/admin/collections",
       icon: <LibraryBig className="h-5 w-5" />,
     },
+    {
+      name: "Admins",
+      link: "/admin/admins",
+      icon: <ShieldCheck className="h-5 w-5" />,
+    },
   ];
   return (
     <section className="flex flex-col gap-7 bg-white border-r px-5 py-3 h-screen overflow-hidden w-[260px]">
@@ -68,7 +78,19 @@ export default function Sidebar() {
         })}
       </ul>
       <div className="flex justify-center">
-        <button className="flex gap-2 items-center px-3 py-2 hover:bg-indigo-100 rounded-xl w-full justify-center ease-soft-spring duration-400 transition-all">
+        <button
+        onClick={async ()=>{
+          try {
+            await toast.promise(signOut(auth), {
+              error: (e) => e?.message,
+              loading: "Loading...",
+              success: "Successfully Logged Out",
+
+            });
+          } catch (error) {
+              toast.error(error?.message);
+          }
+        }} className="flex gap-2 items-center px-3 py-2 hover:bg-indigo-100 rounded-xl w-full justify-center ease-soft-spring duration-400 transition-all">
           <LogOut className="h-5 w-5" /> Logout
         </button>
       </div>
--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,28 +1,43 @@
 "use client";
 
-import { useState } from "react";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
+import { useEffect } from "react";
+import AdminLayout from "./components/AdminLayout";
+import AuthContextProvider, {useAuth} from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
+
 
 export default function Layout({ children }) {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggleSidebar = () => {
-        setIsOpen(!isOpen);
-    }
   return (
-    <main className="relative flex">
-      <div className="hidden md:block">
-        <Sidebar />
-      </div>
-      <div className={`fixed md:hidden
-            ${isOpen ? "translate-x-0" : "-translate-x-[260px]"}
-        `}>
-        <Sidebar />
-      </div>
-      <section className="flex-1 flex flex-col min-h-screen">
-        <Header toggleSidebar={toggleSidebar}/>
-        <section className="flex-1 bg-[#eff3f4]">{children}</section>
-      </section>
-    </main>
+    <>
+      <AuthContextProvider>
+        <AdminChecking>{children}</AdminChecking>
+      </AuthContextProvider>
+    </>
   );
+}
+
+function AdminChecking({ children }) {
+  const {user, isLoading} = useAuth();
+  const router = useRouter()
+  useEffect(()=>{
+    if(!user && !isLoading){
+      router.push("/login");
+    }
+  },[user, isLoading]);
+
+  if(isLoading){
+    return (
+      <div className="h-screen w-screen flex justify-center items-center">
+        <h1>Loading....</h1>
+      </div>
+    )
+  }
+  if(!user){
+    return (
+      <div className="h-screen w-screen flex justify-center items-center">
+        <h1>Please Login First!</h1>
+      </div>
+    )
+  }
+  return <AdminLayout>{children}</AdminLayout>;
 }
