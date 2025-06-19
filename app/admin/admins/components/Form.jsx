7315b8b88ac414ccef82962cf3383fdb3c368078
Abhijeet_15 <118336271+AbhijeetKamalekar15@users.noreply.github.com>
2025-06-19 20:13:45 +0530

    Admin Component Done!!

---
+++ app/admin/admins/components/Form.jsx
@@ -0,0 +1,180 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Button } from "../../../../components/ui/button";
+import toast from "react-hot-toast";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { getAdmin } from "../../../../lib/firestore/admins/read_server";
+import { createNewAdmin, updateAdmin } from "../../../../lib/firestore/admins/write";
+
+export default function Form() {
+  const [data, setData] = useState(null);
+  const [image, setImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+
+  const fetchData = async () => {
+    try {
+      const res = await getAdmin({ id: id });
+      if (!res) {
+        toast.error("Admin Not Found!");
+      } else {
+        setData(res);
+      }
+    } catch (error) {
+      toast.error(error?.message);
+    }
+  };
+
+  useEffect(() => {
+    if (id) {
+      fetchData();
+    }
+  }, [id]);
+
+  const handleData = (key, value) => {
+    setData((prevData) => {
+      return {
+        ...(prevData ?? {}),
+        [key]: value,
+      };
+    });
+  };
+
+  const handleCreate = async () => {
+    setIsLoading(true);
+    try {
+      await createNewAdmin({ data: data, image: image });
+      toast.success("Successfully Created");
+      setData(null);
+      setImage(null);
+    } catch (error) {
+      toast.error(error?.message);
+    }
+    setIsLoading(false);
+  };
+
+  const handleUpdate = async () => {
+    setIsLoading(true);
+    try {
+      await updateAdmin({ data: data, image: image });
+      toast.success("Successfully Updated!");
+      setData(null);
+      setImage(null);
+      router.push(`/admin/admins`);
+    } catch (error) {
+      toast.error(error?.message);
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="flex flex-col gap-3 bg-white rounded-xl p-5 w-full md:w-[400px]">
+      <h1 className="font-semibold">{id ? "Update" : "Create"} Admin</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (id) {
+            handleUpdate();
+          } else {
+            handleCreate();
+          }
+        }}
+        className="flex flex-col gap-2"
+      >
+        <div className="flex flex-col gap-1">
+          <label htmlFor="admin-image" className="text-gray-500 text-sm">
+            Image <span className="text-red-500">*</span>
+          </label>
+          <div className="flex justify-center items-center p-3">
+            {image && <img className="h-35" src={URL.createObjectURL(image)} />}
+          </div>
+          <input
+            id="admin-image"
+            name="admin-image"
+            type="file"
+            onChange={(e) => {
+              if (e.target.files.length > 0) {
+                setImage(e.target.files[0]);
+              }
+            }}
+            className="border px-4 py-2 rounded-lg w-full focus:outline-none"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="admin-name" className="text-gray-500 text-sm">
+            Name <span className="text-red-500">*</span>
+          </label>
+          <input
+            id="admin-name"
+            name="admin-name"
+            type="text"
+            placeholder="Enter Name"
+            value={data?.name ?? ""}
+            onChange={(e) => {
+              handleData("name", e.target.value);
+            }}
+            className="border px-4 py-2 rounded-lg w-full focus:outline-none"
+          />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label htmlFor="admin-email" className="text-gray-500 text-sm">
+            Email <span className="text-red-500">*</span>
+          </label>
+          <input
+            id="admin-email"
+            name="admin-email"
+            type="email"
+            placeholder="Enter Email"
+            value={data?.email ?? ""}
+            onChange={(e) => {
+              handleData("email", e.target.value);
+            }}
+            className="border px-4 py-2 rounded-lg w-full focus:outline-none"
+          />
+        </div>
+        
+        <Button
+          disabled={isLoading}
+          type="submit"
+          className="flex items-center justify-center gap-2"
+        >
+          {isLoading && (
+            <svg
+              className="animate-spin h-4 w-4 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+              ></path>
+            </svg>
+          )}
+          {isLoading
+            ? id
+              ? "Updating..."
+              : "Creating..."
+            : id
+            ? "Update"
+            : "Create"}
+        </Button>
+      </form>
+    </div>
+  );
+}
---
+++ app/admin/admins/components/ListView.jsx
@@ -0,0 +1,124 @@
+"use client";
+
+import { useAdmins } from "../../../../lib/firestore/admins/read";
+import {Button} from "../../../../components/ui/button"
+import { Edit2, Trash2 } from "lucide-react";
+import toast from "react-hot-toast";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { deleteAdmin } from "../../../../lib/firestore/admins/write";
+export default function ListView() {
+  const { data: admins, error, isLoading } = useAdmins();
+  if (isLoading) {
+    return <div>Loading....</div>;
+  }
+  if (error) {
+    return <div>{error}</div>;
+  }
+  return (
+    <div className="flex-1 flex flex-col gap-3 md:pr-5 md:px-0 px-5 rounded-xl ">
+      <h1 className="text-xl">Admins</h1>
+      <table className="border-separate border-spacing-y-3">
+        <thead>
+          <tr>
+            <th className="font-semibold border-y bg-white px-3 py-2 border-l rounded-l-lg">SN</th>
+            <th className="font-semibold border-y bg-white px-3 py-2">Image</th>
+            <th className="font-semibold border-y bg-white px-3 py-2 text-left">Name</th>
+            <th className="font-semibold border-y bg-white px-3 py-2  border-r rounded-r-lg text-center">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {admins?.map((item, index) => {
+            return (
+              <Row index={index} item={item} key={item?.id ?? index} />
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function Row({ item, index }) {
+  const [isDeleting, setIsDeleting] = useState(false);
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    if(!confirm("Are you sure?")) return;
+    setIsDeleting(true);
+    try {
+      await deleteAdmin({ id: item?.id });
+      toast.success("Successfully Deleted");
+    } catch (error) {
+      toast.error(error?.message);
+    }
+    setIsDeleting(false);
+  };
+
+  const handleUpdate = () => {
+    router.push(`/admin/admins?id=${item?.id}`);
+
+  }
+
+  return (
+    <tr>
+      <td className="border-y bg-white px-3 py-2 border-l rounded-l-lg text-center">
+        {index + 1}
+      </td>
+      <td className="border-y bg-white px-3 py-2">
+        <div className="flex justify-center">
+          <img
+            className="h-12 w-12 object-contain rounded-lg"
+            src={item?.imageURL}
+            alt=""
+          />
+        </div>
+      </td>
+      <td className="border-y bg-white px-3 py-2">
+        <div className="flex flex-col">
+          <h2>{item?.name}</h2>
+          <h3 className="text-xs text-blue-500">{item?.email}</h3>
+        </div>
+      </td>
+      <td className="border-y bg-white px-3 py-2 border-r rounded-r-lg">
+        <div className="flex gap-2 items-center justify-center">
+          <Button onClick={handleUpdate} disabled={isDeleting} size="sm">
+            <Edit2 size={13} />
+          </Button>
+          <Button
+            onClick={handleDelete}
+            disabled={isDeleting}
+            size="sm"
+            className="bg-red-500 text-white flex gap-1 items-center justify-center"
+          >
+            {isDeleting ? (
+              <svg
+                className="animate-spin h-4 w-4 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                ></path>
+              </svg>
+            ) : (
+              <Trash2 size={13} />
+            )}
+          </Button>
+        </div>
+      </td>
+    </tr>
+  );
+}
